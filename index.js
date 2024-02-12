432ce2bcf80b0ba31fe21a11de13fcf649cd1a9b
lucca_system <144394390+tatolucca@users.noreply.github.com>
2024-02-12 19:43:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var compras = [];
+var clientes = document.querySelector('.cliente')
+var resultado = document.getElementById('resultado')
+var btn_enviar = document.getElementById('enviar')
+
+
+functionlistar
+btn_enviar.addEventListener('click', (e)=>{
+e.preventDefault
+
+
+})
